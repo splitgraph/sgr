dbeb0bca4c6672767b5bc2a9251ac59da90493fe
Artjoms Iskovs <mildbyte@gmail.com>
2018-07-27 10:30:53 +0100

    pgclient now has the prototype installed in it; added a softlink in the root and moved the actual source code deeper into the client because Docker.

--- test/splitgraph/test_drawing.py
+++
@@ -1,9 +0,0 @@
-from splitgraph.drawing import render_tree
-from splitgraph.sgfile import execute_commands
-from test.splitgraph.test_sgfile import BASE_COMMANDS, sg_pg_mg_conn
-
-
-def test_drawing(sg_pg_mg_conn):
-    # Doesn't really check anything, mostly used to make sure the tree drawing code doesn't throw.
-    execute_commands(sg_pg_mg_conn, BASE_COMMANDS[:5])
-    render_tree(sg_pg_mg_conn, 'output')
