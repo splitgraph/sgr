c511614fdedd5a0c336fca2cf5450bcf377cbfda
Miles Richardson <milesrichardson@gmail.com>
2018-08-20 10:15:40 -0400

    Add support for running `python setup.py test`
    Via https://docs.pytest.org/en/latest/goodpractices.html
    
    * Refactor setup.py to cleanly handle dependencies
    * Add setup.cfg `test` alias to `pytest`
    * Ensure latest setuptools is installed in Dockerfile
      for pgclient and snapper
    
    Note: Within Docker, must explicitly use python3, e.g.
          `python3 setup.py test`

--- setup.py
+++ setup.py
@@ -1,10 +1,29 @@
 from setuptools import setup
 
+install_requirements = [
+    'click',
+    'psycopg2',
+    'requests',
+    'parsimonious'
+]
+
+tests_requirements = [
+    'pytest',
+]
+
+setup_requirements = [
+    'pytest-runner'
+]
+
 setup(
     name="splitgraph-prototype",
     version="0.0",
     packages=['splitgraph'],
     entry_points={
         'console_scripts': ['sg=splitgraph.commandline:cli'],
-    }, install_requires=['click', 'psycopg2', 'requests', 'parsimonious'],
+    },
+    install_requires=install_requirements + tests_requirements,
+    tests_require=tests_requirements,
+    setup_requires=setup_requirements,
+    test_suite='test/splitgraph'
 )
