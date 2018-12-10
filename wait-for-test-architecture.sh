1824a7cfa83c72089112fe96a9aa17059869f2f8
Artjoms Iskovs <mildbyte@gmail.com>
2018-12-10 11:21:22 +0000

    Changed the initialization script to install the audit trigger from sgr -- in preparation for removing it from the driver

--- wait-for-test-architecture.sh
+++ wait-for-test-architecture.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 for i in `seq 10` ; do
+  # Make sure the test drivers are up and install the audit triggers.
+  SG_CONFIG_FILE=test/resources/.sgconfig sgr init
+  SG_CONFIG_FILE=test/resources/.sgconfig SG_DRIVER_PORT=5431 sgr init
+  # Also make sure the 3 upstream databases that we're ingesting data from are up.
   SG_CONFIG_FILE=test/resources/.sgconfig python -c "import test.splitgraph.conftest as c; c.healthcheck()"
   result=$?
   if [ $result -eq 0 ] ; then
