79a1f73a445696aebb5bc6df2ed79c0453c4d731
Artjoms Iskovs <mildbyte@gmail.com>
2019-01-12 20:23:11 +0000

    Managed to get Splitgraph installed into the container and to work with Multicorn

---
+++ engine/build_scripts/build_splitgraph.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Install the Splitgraph library and the layered querying foreign data wrapper.
+# We could use the Postgres entrypoint system instead but it's run as the non-root user
+# and this container isn't for the sgr tool anyway.
+
+cd /splitgraph
+
+ln -s /usr/bin/pip3 /usr/bin/pip
+# Install globally (otherwise we'll need to find a way to get Multicorn to see the venv)
+poetry config settings.virtualenvs.create false
+poetry install --no-dev
