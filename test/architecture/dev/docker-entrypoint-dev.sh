bcb6e8e9f8f15be4964e5ce2bcee3a559285ae23
Miles Richardson <milesrichardson@gmail.com>
2019-03-28 09:22:03 +0000

    Make tests work within the docker dev container
    From within the dev container (if you did `docker-compose ... sgr shell`),
    you can now run `pytest` to run the test suite.
    
    Or, from outside the container, you can run the entire test-suite within
    docker (no need to muck about with host entries or port forwarding)
    
    EXAMPLE:
    
      docker-compose -f docker-compose.yml -f docker-compose.dev.yml run test
    
    This will run the full test suite and exit with the exit code of pytest.
    Any additional arguments will be passed to pytest.
    
    - Add aliases inside the docker container: sgr, poetry, pytest
    - Add custom pytest .ini for when pytest is running in docker, which
      is set as an argument in the pytest alias. This allows using a
      different .sgconfig file when testing in the docker container vs.
      testing in the host
    - In entrypoint of docker container, delete all .pyc files and __pycache__
      directories, because they are tainted by host-specific full paths and
      therefore cause errors when trying to pytest inside docker
    - Now two commands in docker-compose.dev.yml:
        - `sgr` for when you just want to an sgr to play with
          - does not depend on mongoorigin / pgorigin, i.e. "more lightweight
          - if you need those, launch them manually
        - `test` for running the test suite with exit code from pytest

--- test/architecture/dev/docker-entrypoint-dev.sh
+++ test/architecture/dev/docker-entrypoint-dev.sh
@@ -1,21 +1,44 @@
 #!/usr/bin/env bash
 
+main() {
+    ( install_latest_sgr && clear_pycache_files ) || return 1
+    return 0
+}
+
 install_latest_sgr() {
     mkdir -p /src/venvs \
         && poetry config settings.virtualenvs.create true \
         && poetry config settings.virtualenvs.path /src/venvs \
         && cd /src/splitgraph \
-        && poetry install --develop sgr
+        && poetry install -E ingestion --develop splitgraph
+}
+
+# Because we are bind-mounting the root of the repository with /src/splitgraph
+# in the container, _all_ the files are shared. This includes __pycache__ and
+# .pyc files, but these files contain system-specific paths (i.e. full paths
+# of modules on the host machine) that do not match docker paths. Therefore, in
+# order to e.g. run tests in the docker container, we first need to clear all the
+# .pyc and __pycache__ files/directories. Obviously, this will also clear them
+# on the host. But since this is development, that is okay. It just means you
+# cannot run the test suite in docker and on the host in parallel. No surprise.
+clear_pycache_files() {
+    python3 -c "import pathlib; [p.unlink() for p in pathlib.Path('.').rglob('*.py[co]')]" \
+        && python3 -c "import pathlib; [p.rmdir() for p in pathlib.Path('.').rglob('__pycache__')]" \
+        && return 0
+
+    return 1
 }
 
 poetry() {
     "$HOME"/.poetry/bin/poetry "$@"
 }
 
-install_latest_sgr "$@"
+main "$@" || exit 1
 
 if test "$1" = "shell" ; then
     exec "/bin/bash"
+elif test "$1" = "test" ; then
+    exec "$HOME"/.poetry/bin/poetry run pytest -c /sgconfig/pytest.dev.ini "$@"
 else
     exec "$HOME"/.poetry/bin/poetry run sgr "$@"
 fi
