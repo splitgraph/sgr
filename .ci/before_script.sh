8de240a5b124bd226c4db3063e1fdd135fd7d00a
Artjoms Iskovs <mildbyte@gmail.com>
2019-04-12 10:51:13 +0100

    Build, pull and start origin DBs in the background whilst the other tests are running.

--- .ci/before_script.sh
+++ .ci/before_script.sh
@@ -5,17 +5,27 @@ REPO_ROOT_DIR="${CI_DIR}/.."
 TEST_DIR="${REPO_ROOT_DIR}/test"
 ARCHITECTURE_DIR="${TEST_DIR}/architecture"
 
+CORE_ARCHITECTURE="docker-compose.core.yml"
+MOUNTING_ARCHITECTURE="docker-compose.mounting.yml"
+
 # Stop the PG/MySQL that ship with Travis and run our own integration test
 # SG engine/remote architecture instead.
 pushd "$REPO_ROOT_DIR" \
     && ( sudo /etc/init.d/postgresql stop || true ; ) \
     && ( sudo /etc/init.d/mysql stop || true ; ) \
     && pushd "${ARCHITECTURE_DIR}" \
-    && docker-compose pull \
-    && docker-compose build \
-    && docker-compose up -d \
+    && docker-compose -f $CORE_ARCHITECTURE pull \
+    && docker-compose -f $CORE_ARCHITECTURE build \
+    && docker-compose -f $CORE_ARCHITECTURE up -d \
+    && { {
+        echo "Building the mounting test architecture in the background: " $(date)
+        docker-compose -f $MOUNTING_ARCHITECTURE pull
+        docker-compose -f $MOUNTING_ARCHITECTURE build
+        docker-compose -f $MOUNTING_ARCHITECTURE up -d
+        echo "Background mounting test architecture build complete: " $(date)
+    } & } \
     && popd \
-    && echo "Wait for test architecture..." \
+    && echo "Wait for core test architecture..." \
     && pushd "${ARCHITECTURE_DIR}" \
     && ( grep local_engine /etc/hosts >/dev/null || echo "127.0.0.1 local_engine" | sudo tee -a /etc/hosts ; ) \
     && ( grep remote_engine /etc/hosts >/dev/null || echo "127.0.0.1 remote_engine" | sudo tee -a /etc/hosts ; ) \
