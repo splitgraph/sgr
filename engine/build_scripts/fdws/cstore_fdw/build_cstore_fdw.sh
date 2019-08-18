5aaf6ebb9129c83f2ec63ba61b8a1cba5d5bbcb1
Artjoms Iskovs <mildbyte@gmail.com>
2019-08-18 10:53:06 +0100

    Use the develop_v1x branch of cstore_fdw to amongst other things allow parallel scans of CStore fragments (https://github.com/citusdata/cstore_fdw/pull/198)

--- engine/build_scripts/fdws/cstore_fdw/build_cstore_fdw.sh
+++ engine/build_scripts/fdws/cstore_fdw/build_cstore_fdw.sh
@@ -2,6 +2,9 @@
 
 git clone https://github.com/citusdata/cstore_fdw.git
 cd cstore_fdw
+# Check out the current tip of "development" containing amongst other things
+# a patch to make it possible to use cstore tables in parallel scans.
+git checkout develop_v1x
 
 make
 make install
