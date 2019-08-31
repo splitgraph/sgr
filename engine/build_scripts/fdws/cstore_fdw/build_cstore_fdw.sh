53a3603bfb7e81f14651fb2309182431dca08a58
Artjoms Iskovs <mildbyte@gmail.com>
2019-08-31 22:19:15 +0100

    Add a patch to CStore that disables deleting the actual physical files when a table or the database is dropped (in case we spin up an engine to query CStore files directly).

--- engine/build_scripts/fdws/cstore_fdw/build_cstore_fdw.sh
+++ engine/build_scripts/fdws/cstore_fdw/build_cstore_fdw.sh
@@ -6,5 +6,9 @@ cd cstore_fdw
 # a patch to make it possible to use cstore tables in parallel scans.
 git checkout develop_v1x
 
+# Apply a patch that disables CStore deleting the actual physical files when a table
+# or the database is dropped (in case we spin up an engine to query CStore files directly).
+git apply /build_scripts/fdws/cstore_fdw/do_not_delete_objects_on_drop.patch
+
 make
 make install
