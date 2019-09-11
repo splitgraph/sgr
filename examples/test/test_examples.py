6a351fb17091a6e028455fb89c7acdea77575b0b
Artjoms Iskovs <mildbyte@gmail.com>
2019-09-11 14:57:40 +0100

    Fix example tests (use the correct config file so that mongo_fdw gets picked up)

--- examples/test/test_examples.py
+++ examples/test/test_examples.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 
 import pytest
@@ -10,10 +11,16 @@ def test_example(example_path):
 
     :param example_path: Path to the example directory
     """
+
+    # Remove SG-specific envvars that were set in tests (to simulate run_example.py being
+    # actually run in a clean environment, e.g. without SG_CONFIG_FILE set.)
+    env = {k: v for k, v in os.environ.items() if not k.startswith("SG_")}
+
     result = subprocess.run(
         args=["../run_example.py", "example.yaml", "--no-pause"],
         cwd=example_path,
         stderr=subprocess.STDOUT,
+        env=env,
     )
 
     if result.returncode != 0:
