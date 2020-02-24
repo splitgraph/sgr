3292f446ad291feeebc332c6976bcbaf4029441c
Artjoms Iskovs <mildbyte@gmail.com>
2020-02-24 20:28:57 +0000

    Add automatic pushes to Docker engine images after a successful build on master:
      * release builds for tagged commit (e.g. 1.0.0) get pushed under that tag, the short (8 characters) commit SHA, `latest` and `stable`
      * other builds on master get pushed under short commit SHA and `latest`
      * that way `latest` always refers to the latest commit on master (nightly build), every commit can be reached via its SHA, latest release is `stable` and each version has a tag.
    
    Temporarily enable building this on non-master branches to check pushes work correctly.
    
    Also temporarily make example tests verbose and list images they pulled after that to make sure the example tests are run against the freshly built engine.

---
+++ .ci/push_engine.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -ex
+
+# Push the built image out to Docker Hub
+
+docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
+
+# Determine the tags we're pushing the image out under.
+
+# If the Git commit tag is specified, treat this as a release and push
+# the image out with that tag + "stable" + "latest".
+
+# If it's not specified (we're assuming this is only run from master builds),
+# push the image out under "latest".
+
+# We could also do something like what Python does and push out the major, minor and
+# release tags, e.g. a build of 3.4.5 would push out splitgraph/engine:3.4.5,
+# splitgraph/engine:3.4 and splitgraph/engine:latest.
+
+short_sha=$(echo "$TRAVIS_COMMIT" | cut -c-8)
+
+if [ -z "$TRAVIS_TAG" ]; then
+  TAGS="latest $TRAVIS_TAG $short_sha stable"
+else
+  TAGS="latest $short_sha"
+fi
+
+source="$DOCKER_REPO"/"$DOCKER_ENGINE_IMAGE":"$DOCKER_TAG"
+for tag in $TAGS; do
+  target="$DOCKER_REPO"/"$DOCKER_ENGINE_IMAGE":$tag
+  docker tag "$source" "$target"
+  docker push "$target"
+done
