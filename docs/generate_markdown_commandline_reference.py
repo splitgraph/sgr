f1e741a0fa2d0a6e032195948d57ac37845712e8
Artjoms Iskovs <mildbyte@gmail.com>
2020-02-25 22:18:07 +0000

    Change `sgr` reference generator to generate MDX files with expected directory structure and metadata.

--- docs/generate_markdown_commandline_reference.py
+++ docs/generate_markdown_commandline_reference.py
@@ -1,7 +1,7 @@
 """
 Generates the sgr documentation in Markdown format
 """
-
+import json
 import os
 import shutil
 
@@ -21,24 +21,27 @@ from splitgraph.commandline.ingestion import csv_export, csv_import
 
 # Map category to Click commands -- maybe eventually we'll read this dynamically...
 
-STRUCTURE = {
-    "Image management/creation": ["checkout", "commit", "tag", "import", "reindex"],
-    "Image information": ["log", "diff", "object", "objects", "show", "table", "sql", "status"],
-    "Engine management": [
-        "engine add",
-        "engine delete",
-        "engine list",
-        "engine start",
-        "engine stop",
-        "engine log",
-        "engine configure",
-    ],
-    "Data import/export": ["csv export", "csv import", "mount"],
-    "Miscellaneous": ["rm", "init", "cleanup", "prune", "config", "dump", "eval"],
-    "Sharing images": ["clone", "push", "pull", "publish", "upstream"],
-    "Splitfile execution": ["build", "rebuild", "provenance"],
-    "Splitgraph Cloud": ["cloud register", "cloud login", "cloud curl"],
-}
+STRUCTURE = [
+    ("Image management/creation", ["checkout", "commit", "tag", "import", "reindex"]),
+    ("Image information", ["log", "diff", "object", "objects", "show", "table", "sql", "status"]),
+    (
+        "Engine management",
+        [
+            "engine add",
+            "engine delete",
+            "engine list",
+            "engine start",
+            "engine stop",
+            "engine log",
+            "engine configure",
+        ],
+    ),
+    ("Data import/export", ["csv export", "csv import", "mount"]),
+    ("Miscellaneous", ["rm", "init", "cleanup", "prune", "config", "dump", "eval"]),
+    ("Sharing images", ["clone", "push", "pull", "publish", "upstream"]),
+    ("Splitfile execution", ["build", "rebuild", "provenance"]),
+    ("Splitgraph Cloud", ["cloud register", "cloud login", "cloud curl"]),
+]
 
 # Map command names to Click command instances where they don't have the expected (cmd_name + '_c') format
 STRUCTURE_CMD_OVERRIDE = {
@@ -69,10 +72,16 @@ def _emit_argument(argument):
     return argument.make_metavar()
 
 
-def _emit_command_invocation(command, prefix='sgr '):
+def _emit_command_invocation(command, prefix="sgr "):
     # e.g. sgr import [OPTIONS] IMAGE_SPEC ...
-    result = "```" + prefix + command.name + " [OPTIONS] " + \
-             " ".join(_emit_argument(a) for a in command.params if isinstance(a, click.Argument)) + "```\n"
+    result = (
+        "```"
+        + prefix
+        + command.name
+        + " [OPTIONS] "
+        + " ".join(_emit_argument(a) for a in command.params if isinstance(a, click.Argument))
+        + "```\n"
+    )
     return result
 
 
@@ -81,16 +90,28 @@ def _emit_command_options(command):
 
     if help_records:
         result = "\n\n" + _emit_header("Options", level=3) + "\n\n"
-        result += "\n".join("  * **`%s`**: %s" % (option, option_help) for option, option_help in help_records)
+        result += "\n".join(
+            "  * **`%s`**: %s" % (option, option_help) for option, option_help in help_records
+        )
         return result
     else:
         return ""
 
 
+def _emit_mdx_metadata(doc_id, title):
+    return (
+        "export const meta = {id: %s, title: %s};" % (json.dumps(doc_id), json.dumps(title)) + "\n"
+    )
+
+
+def _emit_dir_metadata(title):
+    return json.dumps({"title": title}) + "\n"
+
+
 def _emit_command(command_name):
     command = STRUCTURE_CMD_OVERRIDE.get(command_name)
     if not command:
-        command = getattr(cmd, command_name + '_c')
+        command = getattr(cmd, command_name + "_c")
     result = _emit_header(command_name, level=2) + "\n"
     result += "\n" + _emit_command_invocation(command)
     # Future: move examples under options?
@@ -100,36 +121,40 @@ def _emit_command(command_name):
 
 
 def _slug_section(section):
-    return section.lower().replace(' ', '_').replace('/', '_')
+    return section.lower().replace(" ", "_").replace("/", "_")
 
 
-@click.command(name='main')
-@click.argument('output', default='../docs/sgr', required=False)
-@click.option('-f', '--force', default=False, is_flag=True)
+@click.command(name="main")
+@click.argument("output", default="../docs/sgr", required=False)
+@click.option("-f", "--force", default=False, is_flag=True)
 def main(output, force):
     if os.path.exists(output):
         if not force:
-            raise click.ClickException('%s already exists, pass -f' % output)
+            raise click.ClickException("%s already exists, pass -f" % output)
         else:
             print("Removing %s" % output)
             shutil.rmtree(output)
 
     os.mkdir(output)
 
-    for section, commands in STRUCTURE.items():
-        section_slug = _slug_section(section)
-        section_path = os.path.join(output, section_slug + '.md')
+    for section_no, (section, commands) in enumerate(STRUCTURE):
+        section_slug = "%04d_%s" % (section_no, _slug_section(section))
+        section_path = os.path.join(output, section_slug)
+        os.mkdir(section_path)
+        with open(os.path.join(section_path, "metadata.json"), "w") as f:
+            f.write(_emit_dir_metadata(title=section))
 
-        with open(section_path, 'w') as f:
-            f.write(_emit_document_header(section_slug, section))
-
-            for c in commands:
-                print("Making %s: %s..." % (section_path, c))
-                f.write(_emit_command(c) + "\n\n")
+        for command_no, command in enumerate(commands):
+            doc_id = _slug_section(command)
+            doc_filename = "%04d_%s.mdx" % (command_no, doc_id)
+            command_path = os.path.join(section_path, doc_filename)
+            print("Making %s: %s..." % (command_path, command))
+            with open(command_path, "w") as f:
+                f.write(_emit_mdx_metadata(doc_id=doc_id, title=command))
+                f.write("\n")
+                f.write(_emit_command(command) + "\n")
 
     print("Done.")
-    sidebar_spec = "\"sgr command line client\": [" + ",\n".join('"sgr/' + _slug_section(s) + '"' for s in STRUCTURE) + ']'
-    print(sidebar_spec)
 
 
 if __name__ == "__main__":
