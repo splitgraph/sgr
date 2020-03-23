ebb9c981a1418e826b25b990e3194f6545d3b07b
Artjoms Iskovs <mildbyte@gmail.com>
2020-03-23 17:50:51 +0000

    Add Pip cache to engine build; bump Poetry and speed up poetry install by using pip first.

--- engine/build_scripts/build_splitgraph.sh
+++ engine/build_scripts/build_splitgraph.sh
@@ -1,9 +1,9 @@
-#!/bin/bash -e
+#!/bin/bash -ex
 
 # Install the Splitgraph library and the layered querying foreign data wrapper.
 cd /splitgraph
 
-export POETRY_VERSION=1.0.0
+export POETRY_VERSION=1.0.5
 
 curl -sSL https://raw.githubusercontent.com/sdispater/poetry/master/get-poetry.py | python3.8
 
@@ -11,4 +11,7 @@ curl -sSL https://raw.githubusercontent.com/sdispater/poetry/master/get-poetry.p
 ln -s /usr/bin/python3.8 /usr/bin/python
 source $HOME/.poetry/env
 poetry config virtualenvs.create false
+
+poetry export -f requirements.txt --without-hashes -o requirements.txt && sed -i "/^-e/d" requirements.txt
+pip install -r requirements.txt
 poetry install --no-dev
