645ce856fc32a4414a574648d6d9a830792ba8d1
Artjoms Iskovs <mildbyte@gmail.com>
2020-04-22 10:21:50 +0100

    Rebase Multicorn on master (some bugfixes, actual py3.8/pg12 support, new maintainer merged some plpython patches and released version 1.4.0)

--- engine/build_scripts/fdws/multicorn/build_multicorn.sh
+++ engine/build_scripts/fdws/multicorn/build_multicorn.sh
@@ -2,11 +2,8 @@
 
 cd /src/Multicorn
 
-# Fork of official Multicorn with a few cherry-picked patches:
-# * https://github.com/Kozea/Multicorn/pull/214   (pg11 build)
-# * https://github.com/Kozea/Multicorn/issues/136  (plpython interaction)
-# * scanning directly through cstore_fdw fragments rather than
-#   passing data through python
+# Fork of official Multicorn with the ability to scan directly through cstore_fdw
+# fragments rather than passing data through python.
 
 export DESTDIR=/output/root
 export PYTHON_OVERRIDE=python3.8
@@ -16,5 +13,5 @@ export PYTHON_CONFIG=x86_64-linux-gnu-python3.8-config
 # Include and dynamically link to cstore_fdw
 make \
   CPPFLAGS="-I ../cstore_fdw" \
-  SHLIB_LINK="-L/output/root/usr/local/lib -lcstore_fdw -lpython3.8" \
+  SHLIB_LINK="-L/output/root/usr/local/lib -lcstore_fdw" \
   install
