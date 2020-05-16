9a705f36c27a7dcf8f3a3417c8b91cf6e81a4815
Artjoms Iskovs <mildbyte@gmail.com>
2020-05-16 11:25:49 +0100

    Debugging for asciicast recording

--- .ci/rebuild_asciicasts.sh
+++ .ci/rebuild_asciicasts.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash -ex
 
 CI_DIR=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 REPO_ROOT_DIR="${CI_DIR}/.."
@@ -18,7 +18,7 @@ test -z "$SG_DEMO_SECRET" && { echo "Fatal Error: No SG_DEMO_SECRET set" ; exit 
 # If there are weird config issues, maybe this is the culprit.
 ASCIINEMA_CONFIG="$REPO_ROOT_DIR"/examples/asciinema.sgconfig
 cp "$REPO_ROOT_DIR"/examples/import-from-mongo/.sgconfig "$ASCIINEMA_CONFIG"
-SG_CONFIG_FILE=$ASCIINEMA_CONFIG sgr cloud login-api --api-key "$SG_DEMO_KEY" --api-secret "$SG_DEMO_SECRET"
+SG_CONFIG_FILE=$ASCIINEMA_CONFIG sgr --verbosity DEBUG cloud login-api --api-key "$SG_DEMO_KEY" --api-secret "$SG_DEMO_SECRET"
 
 # TODO the us-election asciicast requires scipy for the last part, consider replacing
 # dbt required by the dbt example
