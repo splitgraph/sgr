b7e7bde01ea0a04cd43a3ed07e57bf12743d4b71
Artjoms Iskovs <mildbyte@gmail.com>
2020-05-21 12:22:54 +0100

    On `sgr cloud login`, add a hint to the password prompt on how to set it for users who logged in via OAuth and don't have it.

--- test/splitgraph/commandline/test_cloud.py
+++ test/splitgraph/commandline/test_cloud.py
@@ -26,8 +26,8 @@ from splitgraph.exceptions import (
 )
 
 _REMOTE = "remote_engine"
-_ENDPOINT = "http://some-auth-service"
-_GQL_ENDPOINT = "http://some-gql-service"
+_ENDPOINT = "http://some-auth-service.example.com"
+_GQL_ENDPOINT = "http://some-gql-service.example.com"
 _SAMPLE_ACCESS = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYmYiOjE1ODA1OTQyMzQsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwiZW1haWwiOiJzb21ldXNlckBleGFtcGxlLmNvbSIsImV4cCI6MTU4MDU5NzgzNCwidXNlcl9pZCI6IjEyM2U0NTY3LWU4OWItMTJkMy1hNDU2LTQyNjY1NTQ0MDAwMCIsImdyYW50IjoiYWNjZXNzIiwidXNlcm5hbWUiOiJzb21ldXNlciIsImlhdCI6MTU4MDU5NDIzNH0.YEuNhqKfFoxHloohfxInSEV9rnivXcF9SvFP72Vv1mDDsaqlRqCjKYM4S7tdSMap5__e3_UTwE_CpH8eI7DdePjMu8AOFXwFHPl34AAxZgavP4Mly0a0vrMsxNJ4KbtmL5-7ih3uneTEuZLt9zQLUh-Bi_UYlEYwGl8xgz5dDZ1YlwTEMsqSrDnXdjl69CTk3vVHIQdxtki4Ng7dZhbOnEdJIRsZi9_VdMlsg2TIU-0FsU2bYYBWktms5hyAAH0RkHYfvjGwIRirSEjxTpO9vci-eAsF8C4ohTUg6tajOcyWz8d7JSaJv_NjLFMZI9mC09hchbQZkw-37CdbS_8Yvw"
 _SAMPLE_REFRESH = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYmYiOjE1NzYzMTk5MTYsImlhdCI6MTU3NjMxOTkxNiwiZW1haWwiOiJzb21ldXNlckBleGFtcGxlLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwicmVmcmVzaF90b2tlbl9zZWNyZXQiOiJzb21lc2VjcmV0IiwiZXhwIjoxNTc4OTExOTE2LCJ1c2VyX2lkIjoiMTIzZTQ1NjctZTg5Yi0xMmQzLWE0NTYtNDI2NjU1NDQwMDAwIiwidXNlcm5hbWUiOiJzb21ldXNlciIsInJlZnJlc2hfdG9rZW5fa2V5Ijoic29tZWtleSIsImdyYW50IjoicmVmcmVzaCJ9.lO3nN3Tmu3twwUjrWsVpBq7nHHEvLnOGXeMkXXv4PRBADUAHyhmmaIPzgccq9XlwpLIexBAxTKJ4GaxSQufKUVLbzAKIMHqxiGTzELY6JMyUvMDHKeKNsq6FdhHxXoKa96fHaDDa65eGcSRSKS3Yr-9sBiANMBJGRbwypYw41gf61pewMA8TXqBmA-mvsBzMUaQNz1DfjkkpHs4SCERPK0GhYSJwDAwK8U3wG47S9k-CQqpq2B99yRRrdSVRzA_lcKe7GlF-Pw6hbRR7xBPBtX61pPME5hFUCPcwYWYXa_KhqEx9IF9edt9UahZuBudaVLmTdKKWgE9M53jQofxNzg"
 _SAMPLE_API_KEY = "abcdef123456"
@@ -353,6 +353,30 @@ def test_commandline_login_api():
     ic.assert_called_once_with("splitgraph_test_engine_", source_config["SG_CONFIG_FILE"])
 
 
+@httpretty.activate(allow_net_connect=False)
+def test_commandline_login_password_prompt():
+    httpretty.register_uri(
+        httpretty.HTTPretty.POST, _ENDPOINT + "/refresh_token", body=_refresh_token_callback
+    )
+
+    source_config = _make_dummy_config_dict()
+
+    runner = CliRunner()
+    with _patch_login_funcs(source_config) as (pc, ic):
+        result = runner.invoke(
+            login_c,
+            args=["--username", "someuser", "--remote", _REMOTE],
+            input="somepassword\n",
+            catch_exceptions=False,
+        )
+        assert result.exit_code == 0
+        # Check the password prompt contains a hint on how to actually obtain it.
+        assert (
+            "Password (visit http://www.example.com/settings/security if you don't have it)"
+            in result.output
+        )
+
+
 def _make_dummy_config_dict():
     # Sanitize the test config so that there isn't a ton of spam
     source_config = create_config_dict()
@@ -369,15 +393,15 @@ def _make_dummy_config_dict():
     [
         (
             ["ns/repo", "table?id=eq.5"],
-            "http://some-query-service/ns/repo/latest/-/rest/table?id=eq.5",
+            "http://some-query-service.example.com/ns/repo/latest/-/rest/table?id=eq.5",
             [],
         ),
         (
             ["ns/repo:image", "/table?id=eq.5"],
-            "http://some-query-service/ns/repo/image/-/rest/table?id=eq.5",
+            "http://some-query-service.example.com/ns/repo/image/-/rest/table?id=eq.5",
             [],
         ),
-        (["ns/repo"], "http://some-query-service/ns/repo/latest/-/rest", [],),
+        (["ns/repo"], "http://some-query-service.example.com/ns/repo/latest/-/rest", [],),
         (
             [
                 "ns/repo",
@@ -385,7 +409,7 @@ def _make_dummy_config_dict():
                 "splitfile",
                 '{"command": "FROM some/repo IMPORT some_table AS alias", "tag": "new_tag"}',
             ],
-            "http://some-query-service/ns/repo/latest/-/splitfile",
+            "http://some-query-service.example.com/ns/repo/latest/-/splitfile",
             [
                 "-H",
                 "Content-Type: application/json",
