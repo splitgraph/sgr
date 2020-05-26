4bc1a9c829790e775ccc91b8bac2bad1719884fa
Artjoms Iskovs <mildbyte@gmail.com>
2020-05-26 14:46:32 +0100

    Downgrade the Python in-engine to 3.7 and bump PG to 12.3 (maintaining the bullseye hack and working around dep incompatibilities is becoming too expensive).

--- engine/build_scripts/build_splitgraph.sh
+++ engine/build_scripts/build_splitgraph.sh
@@ -5,10 +5,10 @@ cd /splitgraph
 
 export POETRY_VERSION=1.0.5
 
-curl -sSL https://raw.githubusercontent.com/sdispater/poetry/master/get-poetry.py | python3.8
+curl -sSL https://raw.githubusercontent.com/sdispater/poetry/master/get-poetry.py | python3.7
 
 # Install globally (otherwise we'll need to find a way to get Multicorn to see the venv)
-ln -s /usr/bin/python3.8 /usr/bin/python
+ln -s /usr/bin/python3.7 /usr/bin/python
 # shellcheck disable=SC1090
 source "$HOME"/.poetry/env
 poetry config virtualenvs.create false
