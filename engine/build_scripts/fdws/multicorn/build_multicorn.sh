e208a87889297dbc750ae0ef2224eb6f2db584c4
Artjoms Iskovs <mildbyte@gmail.com>
2020-05-26 14:46:32 +0100

    Downgrade the Python in-engine to 3.7 and bump PG to 12.3 (maintaining the bullseye hack and working around dep incompatibilities is becoming too expensive).

--- engine/build_scripts/fdws/multicorn/build_multicorn.sh
+++ engine/build_scripts/fdws/multicorn/build_multicorn.sh
@@ -6,12 +6,12 @@ cd /src/Multicorn
 # fragments rather than passing data through python.
 
 export DESTDIR=/output/root
-export PYTHON_OVERRIDE=python3.8
-export PYTHON_CONFIG=x86_64-linux-gnu-python3.8-config
+export PYTHON_OVERRIDE=python3.7
+export PYTHON_CONFIG=x86_64-linux-gnu-python3.7-config
 
 # Do "make CFLAGS=-DDEBUG install" instead to enable debug output for scans.
 # Include and dynamically link to cstore_fdw
 make \
   CPPFLAGS="-I ../cstore_fdw" \
-  SHLIB_LINK="-L/output/root/usr/local/lib -lcstore_fdw -lpython3.8" \
+  SHLIB_LINK="-L/output/root/usr/local/lib -lcstore_fdw -lpython3.7m" \
   install
