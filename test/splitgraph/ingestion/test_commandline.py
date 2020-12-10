f8104032b1ed2c5317e7dd1e06f687663cbd09e9
Artjoms Iskovs <mildbyte@gmail.com>
2020-12-10 11:35:05 +0000

    Fix piping CSV files from stdin where we don't have access to seek() to reset the stream after sampling it for schema inference.
    Use a custom class that lets us hold on to the sample data as we consume the stream and then return data from it first on second read.

--- test/splitgraph/ingestion/test_commandline.py
+++ test/splitgraph/ingestion/test_commandline.py
@@ -1,4 +1,6 @@
 import os
+import shlex
+import subprocess
 from datetime import datetime as dt
 
 import pytest
@@ -6,6 +8,7 @@ from click.testing import CliRunner
 from test.splitgraph.conftest import INGESTION_RESOURCES
 
 from splitgraph.commandline.ingestion import csv_import, csv_export
+from splitgraph.core.types import TableColumn
 
 
 @pytest.mark.parametrize("custom_separator", [False, True])
@@ -27,6 +30,63 @@ def test_import_empty(ingestion_test_repo, custom_separator):
     # Check a line in the table
     assert ingestion_test_repo.run_sql("SELECT * FROM test_table WHERE fruit_id = 4") == [
         (4, dt(2018, 1, 4, 0, 44, 44), "mustard")
+    ]
+
+
+def test_import_stdin(ingestion_test_repo):
+    # Test that piping CSV data from stdin (where we don't support seek()) works
+    # and infers the schema.
+
+    # Grab the RDU weather CSV from the examples directory
+    csv_path = os.path.normpath(
+        os.path.join(
+            INGESTION_RESOURCES, "../../../examples/import-from-csv/rdu-weather-history.csv"
+        )
+    )
+    assert os.path.exists(csv_path)
+
+    # Run the import directly from the shell to make sure Click's CliRunner isn't doing
+    # anything funky with the file + use cat / pipe to make sure we don't just set a file
+    # descriptor as stdin.
+    subprocess.check_call(
+        "cat %s | sgr --verbosity DEBUG "
+        'csv import -k date -t date timestamp --separator ";" %s rdu'
+        % (shlex.quote(csv_path), shlex.quote(ingestion_test_repo.to_schema())),
+        shell=True,
+    )
+
+    assert ingestion_test_repo.run_sql("SELECT COUNT(1) FROM rdu") == [(4633,)]
+    assert ingestion_test_repo.run_sql(
+        "SELECT fastest2minwinddir FROM rdu WHERE date = '2011-04-18'"
+    ) == [(220,)]
+
+    # Check the first few columns to make sure we inferred the schema correctly (remainder are all
+    # Yes/No varchars)
+    assert ingestion_test_repo.engine.get_full_table_schema(ingestion_test_repo.to_schema(), "rdu")[
+        :12
+    ] == [
+        TableColumn(
+            ordinal=1, name="date", pg_type="timestamp without time zone", is_pk=True, comment=None
+        ),
+        TableColumn(ordinal=2, name="temperaturemin", pg_type="numeric", is_pk=False, comment=None),
+        TableColumn(ordinal=3, name="temperaturemax", pg_type="numeric", is_pk=False, comment=None),
+        TableColumn(ordinal=4, name="precipitation", pg_type="numeric", is_pk=False, comment=None),
+        TableColumn(ordinal=5, name="snowfall", pg_type="numeric", is_pk=False, comment=None),
+        TableColumn(ordinal=6, name="snowdepth", pg_type="numeric", is_pk=False, comment=None),
+        TableColumn(ordinal=7, name="avgwindspeed", pg_type="numeric", is_pk=False, comment=None),
+        TableColumn(
+            ordinal=8, name="fastest2minwinddir", pg_type="integer", is_pk=False, comment=None
+        ),
+        TableColumn(
+            ordinal=9, name="fastest2minwindspeed", pg_type="numeric", is_pk=False, comment=None
+        ),
+        TableColumn(
+            ordinal=10, name="fastest5secwinddir", pg_type="integer", is_pk=False, comment=None
+        ),
+        TableColumn(
+            ordinal=11, name="fastest5secwindspeed", pg_type="numeric", is_pk=False, comment=None
+        ),
+        TableColumn(ordinal=12, name="fog", pg_type="character varying", is_pk=False, comment=None),
     ]
 
 
