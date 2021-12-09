217ada4309a8ae7d56c5937e6fb728d1f527ac49
Artjoms Iskovs <mildbyte@gmail.com>
2021-12-09 14:24:47 +0000

    Use `default_flow_style=False` in YAML output
    (bring it back in line with the PyYAML output which adds a line break after
    every dict element)

--- splitgraph/utils/yaml.py
+++ splitgraph/utils/yaml.py
@@ -4,10 +4,11 @@ from ruamel.yaml import YAML
 
 
 def safe_load(stream) -> Any:
-    yaml = YAML(typ="safe", pure=True)
+    yaml = YAML(typ="safe")
     return yaml.load(stream)
 
 
 def safe_dump(obj: Any, stream, **kwargs) -> None:
-    yaml = YAML(typ="safe", pure=True)
+    yaml = YAML(typ="safe")
+    yaml.default_flow_style = False
     yaml.dump(obj, stream, **kwargs)
