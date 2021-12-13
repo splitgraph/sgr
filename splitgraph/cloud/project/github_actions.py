8181c0a1d3b3d611f5dc5a2fe5507cb6561a8648
Artjoms Iskovs <mildbyte@gmail.com>
2021-12-13 15:48:10 +0000

    Wire dbt project generation into `generate_project`.
    Optionally add a final stage to the GHA pipeline running dbt against all loaded
    repos.
    
    Also set repositories as live/not live based on whether they support mount. For
    repositories that don't support mount, run ingestion as previously and use
    `sgr cloud load` to set up metadata. For live repos, use `sgr cloud load` to set
    up metadata and the external data source settings.

--- splitgraph/cloud/project/github_actions.py
+++ splitgraph/cloud/project/github_actions.py
@@ -9,52 +9,93 @@ from splitgraph.cloud.project.utils import get_source_name
 
 
 def generate_job(
-    repository: str, deploy_url: str = "data.splitgraph.com"
+    repository: str, is_live: bool, is_dbt: bool, deploy_url: str = "data.splitgraph.com"
 ) -> Tuple[str, Dict[str, Any]]:
     job_id = get_source_name(repository)
-    job_doc = {
-        "name": f"Build {repository}",
-        "runs-on": "ubuntu-20.04",
-        "steps": [
-            {"uses": "actions/checkout@v2"},
-            {
-                "name": "Set up Splitgraph",
-                "uses": "splitgraph/setup-splitgraph@master",
-                "with": {
-                    "splitgraph_deployment_url": deploy_url,
-                    "splitgraph_api_key": "${{ secrets.SPLITGRAPH_API_KEY }}",
-                    "splitgraph_api_secret": "${{ secrets.SPLITGRAPH_API_SECRET }}",
-                },
+
+    steps = [
+        {"uses": "actions/checkout@v2"},
+        {
+            "name": "Set up Splitgraph",
+            "uses": "splitgraph/setup-splitgraph@master",
+            "with": {
+                "splitgraph_deployment_url": deploy_url,
+                "splitgraph_api_key": "${{ secrets.SPLITGRAPH_API_KEY }}",
+                "splitgraph_api_secret": "${{ secrets.SPLITGRAPH_API_SECRET }}",
             },
+        },
+    ]
+    if is_dbt:
+        steps.append(
             {
-                "name": "Set up credentials for Splitgraph",
+                "name": "Set up dbt Git URL",
+                "run": 'echo "$CREDENTIALS_YML" > splitgraph.credentials.yml'
+                + '\nsed -i "s|\\$THIS_REPO_URL|https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY|g" splitgraph.credentials.yml',
+                "shell": "bash",
+                "env": {
+                    "CREDENTIALS_YML": "${{secrets.SPLITGRAPH_CREDENTIALS_YML}}",
+                    "GITHUB_TOKEN": "${{secrets.GITHUB_TOKEN}}",
+                },
+            }
+        )
+    else:
+        steps.append(
+            {
+                "name": "Set up data source credentials",
                 "run": 'echo "$CREDENTIALS_YML" > splitgraph.credentials.yml',
                 "shell": "bash",
                 "env": {"CREDENTIALS_YML": "${{secrets.SPLITGRAPH_CREDENTIALS_YML}}"},
-            },
+            }
+        )
+
+    if is_live:
+        # For repositories that support mount, we use `sgr cloud load` to set them up
+        # as live data sources and upload metadata.
+        steps.append(
             {
-                "name": "Run sgr cloud sync",
-                "run": f"sgr cloud sync --remote splitgraph {repository} --wait "
-                f"--use-file -f splitgraph.yml -f splitgraph.credentials.yml",
-            },
-        ],
-    }
+                "name": "Run sgr cloud load to set up metadata and data source settings",
+                "run": "sgr cloud load --remote splitgraph "
+                f"-f splitgraph.yml -f splitgraph.credentials.yml {repository}",
+                "shell": "bash",
+            }
+        )
+    else:
+        # For repositories that don't support mount, we only use sgr cloud load to set up
+        # their READMEs etc. Then we use `sgr cloud sync` to actually run ingestion.
+        steps.append(
+            {
+                "name": "Run sgr cloud load to set up metadata",
+                "run": "sgr cloud load --remote splitgraph --skip-external "
+                f"-f splitgraph.yml -f splitgraph.credentials.yml {repository}",
+                "shell": "bash",
+            }
+        )
+        steps.append(
+            {
+                "name": "Run sgr cloud sync to perform the data load",
+                "run": "sgr cloud sync --remote splitgraph --use-file "
+                f"-f splitgraph.yml -f splitgraph.credentials.yml --wait {repository}",
+                "shell": "bash",
+            }
+        )
+
+    job_doc = {"name": f"Build {repository}", "runs-on": "ubuntu-20.04", "steps": steps}
 
     return job_id, job_doc
 
 
 def generate_workflow(
-    repositories: List[str], dependencies: Dict[str, List[str]]
+    repositories: List[Tuple[str, bool, bool]], dependencies: Dict[str, List[str]]
 ) -> Dict[str, Any]:
     job_ids = {}
     job_docs = {}
 
-    for repository in repositories:
-        job_id, job_doc = generate_job(repository)
+    for repository, is_live, is_dbt in repositories:
+        job_id, job_doc = generate_job(repository, is_live, is_dbt)
         job_ids[repository] = job_id
         job_docs[repository] = job_doc
 
-    for repository in repositories:
+    for repository, _, _ in repositories:
         repo_deps = dependencies.get(repository)
         if not repo_deps:
             continue
@@ -64,6 +105,6 @@ def generate_workflow(
     workflow_doc = {
         "name": "Build datasets on Splitgraph",
         "on": "workflow_dispatch",
-        "jobs": {job_ids[r]: job_docs[r] for r in repositories},
+        "jobs": {job_ids[r]: job_docs[r] for r, _, _ in repositories},
     }
     return workflow_doc
