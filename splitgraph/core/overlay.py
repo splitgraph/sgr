d4a46abfb8433aea5e708aeb389be9b50ea0aaae
Marko Grujic <markoog@gmail.com>
2022-04-07 11:09:32 +0000

    Bifurcate overlay component naming convention to support DDN writes

--- splitgraph/core/overlay.py
+++ splitgraph/core/overlay.py
@@ -10,15 +10,25 @@ if TYPE_CHECKING:
 SG_ROW_SEQ = "_sg_row_seq"
 WRITE_LOWER_PREFIX = "_sgov_lower_"
 WRITE_UPPER_PREFIX = "_sgov_upper_"
+WRITE_MERGED_PREFIX = "_sgov_merged"
 
 
 def init_write_overlay(
-    object_engine: "PostgresEngine", schema: str, table: str, table_schema: TableSchema
+    object_engine: "PostgresEngine",
+    schema: str,
+    table: str,
+    table_schema: TableSchema,
+    ddn_layout=False,
 ) -> None:
     from splitgraph.engine.postgres.engine import SG_UD_FLAG
 
     upper_table = WRITE_UPPER_PREFIX + table
-    lower_table = WRITE_LOWER_PREFIX + table
+    # LQ checkout with sgr vs DDN differ in that in the former case the user
+    # interacts with the overlay view directly, and can thus see the all pending
+    # changes at any time. In the latter case, the user interacts only with the
+    # foreign table backed by LQFDW, and the writes are not visible until flushed.
+    lower_table = table if ddn_layout else WRITE_LOWER_PREFIX + table
+    overlay_view = WRITE_MERGED_PREFIX + table if ddn_layout else table
 
     # Create the "upper" table that actual writes will be recorded in (staging area for
     # new objects)
@@ -67,7 +77,7 @@ def init_write_overlay(
         """
         ).format(
             schema=Identifier(schema),
-            table=Identifier(table),
+            table=Identifier(overlay_view),
             pks=pk_cols_s,
             all_cols=all_cols,
             lower=Identifier(lower_table),
@@ -83,7 +93,7 @@ def init_write_overlay(
     for col in table_schema:
         if col.comment:
             query += SQL("COMMENT ON COLUMN {}.{}.{} IS %s;").format(
-                Identifier(schema), Identifier(table), Identifier(col.name)
+                Identifier(schema), Identifier(overlay_view), Identifier(col.name)
             )
             args.append(col.comment)
     if len(args) > 0:
@@ -117,7 +127,7 @@ CREATE TRIGGER {1} INSTEAD OF INSERT OR UPDATE OR DELETE ON {0}.{1}
 """
         ).format(
             Identifier(schema),
-            Identifier(table),
+            Identifier(overlay_view),
             Identifier(upper_table),
         )
     )
