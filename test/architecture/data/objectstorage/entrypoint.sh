a50d6897be964ec4c93c07e6feb1c35f0957d6bc
Marko Grujic <markoog@gmail.com>
2022-06-06 14:07:50 +0200

    Further simplify minio test container config

--- test/architecture/data/objectstorage/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -ex
-
-echo "Copying the CSV test files into the bucket"
-rm /data/test_csv -rf
-cp -r /objectstorage/test_csv /data/test_csv
-
-echo "Starting Minio"
-exec /usr/bin/docker-entrypoint.sh server /data
