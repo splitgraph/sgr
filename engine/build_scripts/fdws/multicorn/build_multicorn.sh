8481827577b33ce4bdf81481e29950c314adcf68
Artjoms Iskovs <mildbyte@gmail.com>
2023-02-27 16:56:09 +0000

    Upgrade the engine Python to 3.9

--- engine/build_scripts/fdws/multicorn/build_multicorn.sh
+++ engine/build_scripts/fdws/multicorn/build_multicorn.sh
@@ -6,12 +6,13 @@ cd /src/Multicorn
 # fragments rather than passing data through python.
 
 export DESTDIR=/output/root
-export PYTHON_OVERRIDE=python3.7
-export PYTHON_CONFIG=x86_64-linux-gnu-python3.7-config
+export PYTHON_OVERRIDE=python3.9
+export PYTHON_CONFIG=x86_64-linux-gnu-python3.9-config
+ln -sf /usr/bin/python3.9 /usr/bin/python
 
 # Do "make CFLAGS=-DDEBUG install" instead to enable debug output for scans.
 # Include and dynamically link to cstore_fdw
 make \
   CPPFLAGS="-I ../cstore_fdw" \
-  SHLIB_LINK="-L/output/root/usr/local/lib -lcstore_fdw -lpython3.7m" \
+  SHLIB_LINK="-L/output/root/usr/local/lib -lcstore_fdw -lpython3.9" \
   install
